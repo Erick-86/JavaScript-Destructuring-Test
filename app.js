9ee51fb615f99f6b05fea2922641edd0bb9f32f2
FreeMan <zelonaire@gmail.com>
2023-03-21 14:10:41 +0000

    question 4 solution

--- app.js
+++ app.js
@@ -41,55 +41,59 @@ GOOD LUCK �
 
 */
 
- const game = {
-   team1: "Bayern Munich",
-   team2: "Borrussia Dortmund",
-   players: [
-     [
-       "Neuer",
-       "Pavard",
-       "Martinez",
-       "Alaba",
-       "Davies",
-       "Kimmich",
-       "Goretzka",
-       "Coman",
-       "Muller",
-       "Gnarby",
-       "Lewandowski",
-     ],
-     [
-       "Burki",
-       "Schulz",
-       "Hummels",
-       "Akanji",
-       "Hakimi",
-       "Weigl",
-       "Witsel",
-       "Hazard",
-       "Brandt",
-       "Sancho",
-       "Gotze",
-     ],
-   ],
-   score: "4:0",
-   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-   date: "Nov 9th, 2037",
-   odds: {
-     team1: 1.33,
-     x: 3.25,
-     team2: 6.5,
-   },
- };
- 
- //QUESTION 1 SOLUTION
- const players1 = [...game.players[0]]
- const players2 = [...game.players[1]];
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
- //QUESTION 2 SOLUTION
-const [goalKeeper, ...fieldPlayers] = players1
-console.log(goalKeeper, fieldPlayers)
- 
- //QUESTION 3 SOLUTION
-const allPlayers = [...game.players[0], ...game.players[1]]
- console.log(allPlayers)
+//QUESTION 1 SOLUTION
+const players1 = [...game.players[0]];
+const players2 = [...game.players[1]];
+
+//QUESTION 2 SOLUTION
+const [goalKeeper, ...fieldPlayers] = players1;
+console.log(goalKeeper, fieldPlayers);
+
+//QUESTION 3 SOLUTION
+const allPlayers = [...game.players[0], ...game.players[1]];
+console.log(allPlayers);
+
+//QUESTION 4 SOLUTION
+const players1Final = [...game.players[0], "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final)
